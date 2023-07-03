894352d9743af9e0c03c8b7e4dc78319c40c68ac
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2023-07-03 16:58:33 -0500

    [FIX] update Acces control

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const socketIO = require("socket.io");
 const verifyToken = require("./middleware/verifyToken");
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cors());
 app.use(bodyParser.json());
 
